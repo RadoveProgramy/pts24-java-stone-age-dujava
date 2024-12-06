f1538e1f7778ae62f4051f32b72f633a3bed5b13
Rado <dujava.radoslav29@gmail.com>
2024-12-06 07:12:02 +0100

    Added CivilizationCardDeck class

--- src/main/java/sk/uniba/fmph/dcs/game_board/Throw.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/Throw.java
@@ -2,9 +2,9 @@ package sk.uniba.fmph.dcs.game_board;
 
 public final class Throw {
 
-    private final int dieFaces = 6;
+    private static final int dieFaces = 6;
 
-    public int[] throwDice(final int dice) {
+    public static int[] throwDice(final int dice) {
         int[] results = new int[dice];
         for (int i = 0; i < results.length; i++) {
             results[i] = (int) (Math.random() * dieFaces) + 1;
