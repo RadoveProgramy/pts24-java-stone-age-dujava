3677ff69c56a7ef7c01f40f48bd914b5286e20be
Rado <dujava.radoslav29@gmail.com>
2024-12-25 23:57:05 +0100

    CivilizationCardPlace almost completely implemented

--- src/main/java/sk/uniba/fmph/dcs/game_board/CivilizationCardPlace.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/CivilizationCardPlace.java
@@ -1,18 +1,24 @@
 package sk.uniba.fmph.dcs.game_board;
 
-import sk.uniba.fmph.dcs.stone_age.ActionResult;
-import sk.uniba.fmph.dcs.stone_age.Effect;
-import sk.uniba.fmph.dcs.stone_age.HasAction;
-import sk.uniba.fmph.dcs.stone_age.PlayerOrder;
+import sk.uniba.fmph.dcs.stone_age.*;
 
-public class CivilizationCardPlace implements InterFaceFigureLocationInternal{
+import java.util.Optional;
 
-    private PlayerOrder figures;
+public class CivilizationCardPlace implements InterFaceFigureLocationInternal {
+
+    private PlayerOrder[] figures;
     private int requiredResources;
+    private CivilizationCardDeck civilizationCardDeck;
+    private Optional<CivilisationCard> civilisationCard;
+    private RewardMenu rewardMenu;
 
-    public CivilizationCardPlace(final int requiredResources, final PlayerOrder figures) {
-        this.figures = figures;
+    public CivilizationCardPlace(final int requiredResources, final PlayerOrder[] playerOrder,
+                                 CivilizationCardDeck civilizationCardDeck, RewardMenu rewardMenu) {
         this.requiredResources = requiredResources;
+        this.figures = playerOrder;
+        this.civilizationCardDeck = civilizationCardDeck;
+        this.civilisationCard = civilizationCardDeck.getTop();
+        this.rewardMenu = rewardMenu;
     }
 
     /**
@@ -26,27 +32,51 @@ public class CivilizationCardPlace implements InterFaceFigureLocationInternal{
 
     @Override
     public boolean placeFigures(Player player, int figureCount) {
+        if(tryToPlaceFigures(player, figureCount) != HasAction.NO_ACTION_POSSIBLE){
+            int length = figures.length;
+            figures[length] = player.playerOrder();
+            player.playerBoard().takeFigures(1);
+        }
         return false;
     }
 
     @Override
     public HasAction tryToPlaceFigures(Player player, int count) {
-        return null;
+        if(figures.length > 0 || count != 1 || !player.playerBoard().hasFigures(count)){
+            return HasAction.NO_ACTION_POSSIBLE;
+        }
+        return HasAction.WAITING_FOR_PLAYER_ACTION;
     }
 
     @Override
     public ActionResult makeAction(Player player, Effect[] inputResources, Effect[] outputResources) {
-        return null;
+
     }
 
     @Override
     public boolean skipAction(Player player) {
+        for (int i = 0; i < figures.length; i++) {
+            if(figures[i] == player.playerOrder()){
+                figures[i] = null;
+                player.playerBoard().takeFigures(-1);
+                return true;
+            }
+        }
         return false;
+
     }
 
     @Override
     public HasAction tryToMakeAction(Player player) {
-        return null;
+        if(this.civilisationCard != null){
+            for (int i = 0; i < figures.length; i++) {
+                if(figures[i] == player.playerOrder()){
+                    return HasAction.NO_ACTION_POSSIBLE;
+                }
+            }
+            return HasAction.WAITING_FOR_PLAYER_ACTION;
+        }
+        return HasAction.NO_ACTION_POSSIBLE;
     }
 
     @Override
