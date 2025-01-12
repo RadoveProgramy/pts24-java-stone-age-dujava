b52a6b62840af6198dab3efc68739dea471d5dd3
Rado <dujava.radoslav29@gmail.com>
2025-01-12 14:58:45 +0100

    Added ThrowMock

---
+++ src/main/java/sk/uniba/fmph/dcs/game_board/ThrowMock.java
@@ -0,0 +1,8 @@
+package sk.uniba.fmph.dcs.game_board;
+
+public class ThrowMock implements InterfaceThrow{
+    @Override
+    public int[] throwDice(int dice) {
+        return new int[]{};
+    }
+}
